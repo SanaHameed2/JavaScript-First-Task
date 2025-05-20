f589aebfb2717ea28d1d0bdc6f4c304bfc3da10b
SanaHameed2 <sana.faiza2@gmail.com>
2025-05-20 18:56:27 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// var name ="Sana Hameed";
+// var age = 37;
+// var rollNo = 12345;
+// var hobbies = ["reading", "writing", "coding"];
+// alert("My name is : "+ name + "\n"+ rollNo + '\n' + "my age is: " + age + "\nhobbies: " + hobbies.join(",") );//keyword
+var name = "Marin";
+var characterrole = "Deep Watcher";
+var specialmove = "Mirror Bubble";
+alert("Hello from the deep sea! My name is " + name + "\n" + characterrole + "\n" + "My special move? " + specialmove + " — it traps the enemy in a bubble and makes them unable to move for 5 seconds.");
+
+
+
+16-02-2025
+
+var
